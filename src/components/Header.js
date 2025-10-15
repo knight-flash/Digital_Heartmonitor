48b1e87a4322042ad5d3be9428516459f4970783
qwercsj123 <2320049251@qq.com>
2025-10-15 11:14:32 +0800

    test3

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
         <div className="header-left">
           <div className="logo">
             <span className="logo-icon">💬</span>
-            <span className="logo-text">数字心脏监测系统2</span>
+            <span className="logo-text">数字心脏监测系统3</span>
           </div>
         </div>
         
@@ -50,7 +50,7 @@ const Header = () => {
             <div className="institution-line">北京大学健康医疗大数据国家研究院</div>
           </div>
         </div>
-      </div>
+      </div>S
     </header>
   );
 };
